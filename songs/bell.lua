0d88559bd03177305566b8c2c96d8a381d52f62b
Haos <6866961+Megahaosguy@users.noreply.github.com>
2022-08-05 17:42:02 -0500

    More complex song

--- songs/bell.lua
+++ songs/bell.lua
@@ -1,7 +1,8 @@
-song = {}
-song[1] = {"bass", 3, 24, .25}
-song[2] = {"bass", 3, 16, .25}
-song[3] = {"bass", 3, 28, .25}
-song[4] = {"flute", 3, 4, .25}
-song[5] = {"bass", 3, 24, .25}
-song[6] = {"bass", 3, 24, .25}
+track = {}
+track[1] = {"bass", 3, 24, .25}
+track[2] = {"bass", 3, 16, .25}
+track[3] = {"bass", 3, 28, .25}
+track[4] = {"flute", 3, 4, .25}
+track[5] = {"bass", 3, 24, .25}
+track[6] = {"bass", 3, 24, .25}
+song = {"bell", track}
