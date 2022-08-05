5cc765f4fcfb5b60705b909b6f4f40443b9ca962
MS <62676614+MSCompSci@users.noreply.github.com>
2022-08-05 19:51:35 -0400

    Complete first measure and test second

--- songs/Fortnite.lua
+++ songs/Fortnite.lua
@@ -1,7 +1,20 @@
 track = {}
-track[1] = {"bass", 3, 0, .5}
-track[2] = {"bass", 3, 0, .0625}
-track[3] = {"bass", 3, 3, .0625}
-track[4] = {"bass", 3, 5, .0625}
-track[4] = {"bass", 3, 5, .1875}
+track[1] = {"bass", 3, 2, .5}
+track[2] = {"bass", 3, 2, .125}
+track[3] = {"bass", 3, 5, .125}
+track[4] = {"bass", 3, 7, .25}
+track[5] = {"bass", 3, 7, .375}
+track[6] = {"bass", 3, 5, .875}
+
+track[7] = {"bass", 3, 2, .5}
+track[8] = {"bass", 3, 2, .125}
+track[9] = {"bass", 3, 5, .125}
+track[10] = {"bass", 3, 7, .25}
+track[11] = {"bass", 3, 7, .375}
+track[12] = {"bass", 3, 5, .875}
+track[13] = {"bass", 3, 2, .125}
+track[14] = {"bass", 3, 0, .125}
+track[15] = {"bass", 3, 2, .125}
+
+
 song = {"Fortnite", track}
