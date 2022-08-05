69a84c70663e1657fe2efb24fca8d0e2037eb58b
Haos <6866961+Megahaosguy@users.noreply.github.com>
2022-08-05 17:41:14 -0500

    Client now knows what the song is

--- client.lua
+++ client.lua
@@ -6,9 +6,9 @@ print("Client Started")
 while true do
   -- Wait for a message to arrive...
   local event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
-  print("Playing song")
+  print("Playing "..message[1])
   -- Play the encoded message
-  for i,v in ipairs(message) do
+  for i,v in ipairs(message[2]) do
     speaker.playNote(v[1], v[2], v[3])
     sleep(v[4])
   end
