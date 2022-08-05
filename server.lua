cad82f4e65ae316fd1d4e545ecd63b441ca8f583
Haos <6866961+Megahaosguy@users.noreply.github.com>
2022-08-05 17:48:38 -0500

    Fixed require and if statements

--- server.lua
+++ server.lua
@@ -7,7 +7,7 @@ if arg[1] then
   end
   songname = arg[1]
 end
-require songname
+require(songname)
 
 local modem = peripheral.find("modem")
 print("Server Started")
@@ -16,7 +16,7 @@ while true do
   -- Wait for a message to arrive...
   -- local event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
   os.pullEvent("redstone")
-  if rs.getInput("right") do
+  if rs.getInput("right") then
       modem.transmit(89, 1, song)
       print("Sending "..songname)
       sleep(1)
