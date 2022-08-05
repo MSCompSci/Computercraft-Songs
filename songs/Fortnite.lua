2eda118d0299291bf6c06f921187a8a226c9d734
MS <62676614+MSCompSci@users.noreply.github.com>
2022-08-05 19:03:36 -0400

    Add first Fortnite test version

---
+++ songs/Fortnite.lua
@@ -0,0 +1,7 @@
+track = {}
+track[1] = {"bass", 3, 0, .25}
+track[2] = {"bass", 3, 0, .0625}
+track[3] = {"bass", 3, 3, .0625}
+track[4] = {"bass", 3, 5, .0625}
+track[4] = {"bass", 3, 5, .1875}
+song = {"Fortnite", track}
