177f79940943152d107cb4358e9e9d30da7fc351
MS <62676614+MSCompSci@users.noreply.github.com>
2022-08-05 21:24:14 -0400

    Create first stanza of song

---
+++ songs/Never_Gonna.lua
@@ -0,0 +1,37 @@
+track = {}
+track[1] = {"piano", 3, 6, .25}
+track[2] = {"piano", 3, 8, .25}
+track[3] = {"piano", 3, 11, .25}
+track[4] = {"piano", 3, 8, .25}
+track[5] = {"piano", 3, 15, .375}
+track[6] = {"piano", 3, 15, .375}
+track[7] = {"piano", 3, 11, .375}
+
+track[8] = {"piano", 3, 6, .25}
+track[9] = {"piano", 3, 8, .25}
+track[10] = {"piano", 3, 11, .25}
+track[11] = {"piano", 3, 8, .25}
+track[12] = {"piano", 3, 13, .375}
+track[13] = {"piano", 3, 13, .375}
+track[14] = {"paino", 3, 11, .25}
+track[14] = {"piano", 3, 10, .25}
+track[15] = {"piano", 3, 8, .25}
+
+track[16] = {"piano", 3, 6, .25}
+track[17] = {"piano", 3, 8, .25}
+track[18] = {"piano", 3, 11, .25}
+track[19] = {"piano", 3, 8, .25}
+track[20] = {"piano", 3, 11, .25}
+track[21] = {"piano", 3, 13, .25}
+track[22] = {"piano", 3, 10, .375}
+track[23] = {"piano", 3, 8, .25}
+track[24] = {"piano", 3, 6, .25}
+track[25] = {"piano", 3, 6, .25}
+track[26] = {"piano", 3, 6, .25}
+track[27] = {"piano", 3, 13, .25}
+track[28] = {"piano", 3, 11, .25}
+
+
+
+
+song = {"Nyan Cat", track}
