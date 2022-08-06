b9ff17b99db107d1cffd6c34d597c0e5f22b7bb9
Haos <6866961+Megahaosguy@users.noreply.github.com>
2022-08-05 20:59:51 -0500

    Create server_pocket.lua

---
+++ server_pocket.lua
@@ -0,0 +1,23 @@
+-- The Song you want to play
+local songname = "songs/bell"
+if arg[1] then
+  if not fs.exists(arg[1]..".lua") then
+    print(arg[1].." doesn't exist")
+    exit()
+  end
+  songname = arg[1]
+end
+require(songname)
+
+local modem = peripheral.find("modem")
+print("Server Started with song: "..songname)
+
+while true do
+  -- Wait for a message to arrive...
+  print("Press D to send command")
+  local event, key, is_held = os.pullEvent("key")
+  if key == keys.d then
+      modem.transmit(89, 1, song)
+      print("Sending "..songname)
+      sleep(1)
+  end
