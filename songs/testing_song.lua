0ab864e4ad374a2cc4c1cd15e99f3d28825b8a90
MS <62676614+MSCompSci@users.noreply.github.com>
2022-08-06 14:46:02 -0400

    Create testing song for new note decoding system

---
+++ songs/testing_song.lua
@@ -0,0 +1,11 @@
+-- Test song sample
+track = {}
+track[1] = {"bass", 3, "C1", .5}
+track[2] = {"bass", 3, "D2", .125}
+track[3] = {"bass", 3, "Gb2", .125}
+
+-- song format is song name, song array, tempo multiplier
+-- named message to emulate incoming streamed format
+-- tempo multiplier: standard is 1
+-- make greater than 1 for longer, less for shorter
+message = {"testing_song", track, 1}
