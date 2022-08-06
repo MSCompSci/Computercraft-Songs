5b28720fee8496303eb9491ca359471e9966941b
MS <62676614+MSCompSci@users.noreply.github.com>
2022-08-05 21:28:39 -0400

    Update instrument to valid choice

--- songs/Never_Gonna.lua
+++ songs/Never_Gonna.lua
@@ -1,35 +1,35 @@
 track = {}
-track[1] = {"piano", 3, 6, .25}
-track[2] = {"piano", 3, 8, .25}
-track[3] = {"piano", 3, 11, .25}
-track[4] = {"piano", 3, 8, .25}
-track[5] = {"piano", 3, 15, .375}
-track[6] = {"piano", 3, 15, .375}
-track[7] = {"piano", 3, 11, .375}
+track[1] = {"pling", 3, 6, .25}
+track[2] = {"pling", 3, 8, .25}
+track[3] = {"pling", 3, 11, .25}
+track[4] = {"pling", 3, 8, .25}
+track[5] = {"pling", 3, 15, .375}
+track[6] = {"pling", 3, 15, .375}
+track[7] = {"pling", 3, 11, .375}
 
-track[8] = {"piano", 3, 6, .25}
-track[9] = {"piano", 3, 8, .25}
-track[10] = {"piano", 3, 11, .25}
-track[11] = {"piano", 3, 8, .25}
-track[12] = {"piano", 3, 13, .375}
-track[13] = {"piano", 3, 13, .375}
-track[14] = {"paino", 3, 11, .25}
-track[14] = {"piano", 3, 10, .25}
-track[15] = {"piano", 3, 8, .25}
+track[8] = {"pling", 3, 6, .25}
+track[9] = {"pling", 3, 8, .25}
+track[10] = {"pling", 3, 11, .25}
+track[11] = {"pling", 3, 8, .25}
+track[12] = {"pling", 3, 13, .375}
+track[13] = {"pling", 3, 13, .375}
+track[14] = {"pling", 3, 11, .25}
+track[14] = {"pling", 3, 10, .25}
+track[15] = {"pling", 3, 8, .25}
 
-track[16] = {"piano", 3, 6, .25}
-track[17] = {"piano", 3, 8, .25}
-track[18] = {"piano", 3, 11, .25}
-track[19] = {"piano", 3, 8, .25}
-track[20] = {"piano", 3, 11, .25}
-track[21] = {"piano", 3, 13, .25}
-track[22] = {"piano", 3, 10, .375}
-track[23] = {"piano", 3, 8, .25}
-track[24] = {"piano", 3, 6, .25}
-track[25] = {"piano", 3, 6, .25}
-track[26] = {"piano", 3, 6, .25}
-track[27] = {"piano", 3, 13, .25}
-track[28] = {"piano", 3, 11, .25}
+track[16] = {"pling", 3, 6, .25}
+track[17] = {"pling", 3, 8, .25}
+track[18] = {"pling", 3, 11, .25}
+track[19] = {"pling", 3, 8, .25}
+track[20] = {"pling", 3, 11, .25}
+track[21] = {"pling", 3, 13, .25}
+track[22] = {"pling", 3, 10, .375}
+track[23] = {"pling", 3, 8, .25}
+track[24] = {"pling", 3, 6, .25}
+track[25] = {"pling", 3, 6, .25}
+track[26] = {"pling", 3, 6, .25}
+track[27] = {"pling", 3, 13, .25}
+track[28] = {"pling", 3, 11, .25}
 
 
 
