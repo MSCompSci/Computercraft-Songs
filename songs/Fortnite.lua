b13bd5115fc13a9355a7eba249f6142bfa3ae5fb
MS <62676614+MSCompSci@users.noreply.github.com>
2022-08-05 20:05:16 -0400

    Finalize Fortnite timing

--- songs/Fortnite.lua
+++ songs/Fortnite.lua
@@ -1,4 +1,5 @@
 track = {}
+
 track[1] = {"bass", 3, 2, .5}
 track[2] = {"bass", 3, 2, .125}
 track[3] = {"bass", 3, 5, .125}
@@ -11,8 +12,8 @@ track[8] = {"bass", 3, 2, .125}
 track[9] = {"bass", 3, 5, .125}
 track[10] = {"bass", 3, 7, .25}
 track[11] = {"bass", 3, 7, .375}
-track[12] = {"bass", 3, 5, .875}
-track[13] = {"bass", 3, 2, .125}
+track[12] = {"bass", 3, 5, .25}
+track[13] = {"bass", 3, 2, .25}
 track[14] = {"bass", 3, 0, .125}
 track[15] = {"bass", 3, 2, .125}
 
