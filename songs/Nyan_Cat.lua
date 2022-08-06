d68ead56c05a30f3d0a48748bc5859ba3f46ffdc
MS <62676614+MSCompSci@users.noreply.github.com>
2022-08-05 20:49:53 -0400

    Create first measure

---
+++ songs/Nyan_Cat.lua
@@ -0,0 +1,16 @@
+track = {}
+--.125 is 1/16 note
+
+track[1] = {"banjo", 3, 12, .25}
+track[2] = {"banjo", 3, 14, .25}
+track[3] = {"banjo", 3, 8, .125}
+track[4] = {"banjo", 3, 9, .25}
+
+track[5] = {"banjo", 3, 8, .125}
+track[6] = {"banjo", 3, 9, .125}
+track[7] = {"banjo", 3, 7, .125}
+track[8] = {"banjo", 3, 5, .25}
+track[9] = {"banjo", 3, 5, .25}
+track[7] = {"banjo", 3, 7, .25}
+
+song = {"Nyan Cat", track}
