626d453524fcf12002059167f63766cb9c8f447c
Haos <6866961+Megahaosguy@users.noreply.github.com>
2022-08-05 21:01:03 -0500

    Create derpy.lua

---
+++ songs/derpy.lua
@@ -0,0 +1,13 @@
+track = {}
+table.insert(track, {"pling", 3, 24, .25})
+table.insert(track, {"bass", 3, 22, .25})
+table.insert(track, {"pling", 3, 24, .20})
+table.insert(track, {"bass", 3, 22, .18})
+table.insert(track, {"pling", 3, 24, .125})
+table.insert(track, {"guitar", 3, 12, .125})
+table.insert(track, {"pling", 3, 22, .125})
+table.insert(track, {"pling", 3, 21, .125})
+table.insert(track, {"pling", 3, 20, .125})
+table.insert(track, {"pling", 3, 19, .125})
+table.insert(track, {"pling", 3, 18, .125})
+song = {"derpy", track}
