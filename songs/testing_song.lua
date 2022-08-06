9bb9bb80c9776cd950d43a4e970630419a981fe1
Haos <6866961+Megahaosguy@users.noreply.github.com>
2022-08-06 14:36:25 -0500

    Added note notation usage boolean to message

--- songs/testing_song.lua
+++ songs/testing_song.lua
@@ -4,8 +4,8 @@ track[1] = {"bass", 3, "C1", .5}
 track[2] = {"bass", 3, "D2", .125}
 track[3] = {"bass", 3, "Gb2", .125}
 
--- song format is song name, song array, tempo multiplier
+-- song format is song name, song array, tempo multiplier, note notation usage
 -- named message to emulate incoming streamed format
 -- tempo multiplier: standard is 1
 -- make greater than 1 for longer, less for shorter
-message = {"testing_song", track, 1}
+message = {"testing_song", track, 1, true}
