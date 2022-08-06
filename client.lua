f7ddebc5b72b5b992f693f49ad7b2325198d3565
MS <62676614+MSCompSci@users.noreply.github.com>
2022-08-06 14:57:06 -0400

    Change client to new decoding method
    Add support for decoder module so songs can use note names like "C#1" instead of minecraft noteblock numbers

--- client.lua
+++ client.lua
@@ -1,16 +1,30 @@
+-- require decoder script
+local d = require "decoder"
+
 local modem = peripheral.find("modem")
 local speaker = peripheral.find("speaker")
 modem.open(89)
 print("Client Started")
 
+
 while true do
   -- Wait for a message to arrive...
   local event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
-  print("Playing "..message[1])
+  print("Playing "..message[1],"at tempo multiplier"..message[3])
+  
   -- Play the encoded message
   for i,v in ipairs(message[2]) do
-    speaker.playNote(v[1], v[2], v[3])
-    sleep(v[4])
+
+    -- find corresponding mc note number using decoder module
+    -- v[3] is string version of note from song file
+    -- new from McDonalds it's the mcNote!
+    local mcNote = d.findNote(v[3])
+
+    --play song note
+    speaker.playNote(v[1], v[2], mcNote)
+
+    --pause after note controlled by tempo multiplier
+    sleep(v[4] * message[3])
   end
 end
   
