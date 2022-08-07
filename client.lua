d6cc677bf277f2aa7d714c7f80f3dddc7850a1e6
Haos <6866961+Megahaosguy@users.noreply.github.com>
2022-08-06 20:55:19 -0500

    CC doesn't support labels/goto

--- client.lua
+++ client.lua
@@ -8,7 +8,6 @@ print("Client Started")
 
 
 while true do
-  ::main_loop_start::
   -- Wait for a message to arrive...
   local event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
   -- Special Case: Execute shell commands given in message[2]
@@ -19,31 +18,28 @@ while true do
     for i,v in ipairs(message[2]) do
       shell.run(v)
     end
-  -- Restart the while loop
-  goto main_loop_start
-  end
-    
-    
-  -- If nothing special is sent, just play the song received
-  print("Playing "..message[1].." at tempo multiplier"..message[3])
-  
-  -- Play the encoded message
-  for i,v in ipairs(message[2]) do
+  else
+    -- If nothing special is sent, just play the song received
+    print("Playing "..message[1].." at tempo multiplier"..message[3])
 
-    -- find corresponding mc note number using decoder module if the track uses note notation
-    -- v[3] is string version of note from song file
-    -- new from McDonalds it's the mcNote!
-    if v[4] then
-      local mcNote = d.findNote(v[3])
-    else
-      local mcNote = v[3]
+    -- Play the encoded message
+    for i,v in ipairs(message[2]) do
+
+      -- find corresponding mc note number using decoder module if the track uses note notation
+      -- v[3] is string version of note from song file
+      -- new from McDonalds it's the mcNote!
+      if v[4] then
+        local mcNote = d.findNote(v[3])
+      else
+        local mcNote = v[3]
+      end
+
+      --play song note
+      speaker.playNote(v[1], v[2], mcNote)
+
+      --pause after note controlled by tempo multiplier
+      sleep(v[4] * message[3])
     end
-
-    --play song note
-    speaker.playNote(v[1], v[2], mcNote)
-
-    --pause after note controlled by tempo multiplier
-    sleep(v[4] * message[3])
   end
 end
   
