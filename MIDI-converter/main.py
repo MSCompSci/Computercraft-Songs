a0e97a40e4e544bb91625cafcad2ee36ae192d3f
MS <62676614+MSCompSci@users.noreply.github.com>
2022-08-07 23:35:45 -0400

    Update main.py to add even more functionality
    Add converted midi Minecraft formatting functionality and lua file export functionality

--- MIDI-converter/main.py
+++ MIDI-converter/main.py
@@ -1,15 +1,34 @@
 from mido import MidiFile
 import music21
+import os
 
-#Global Variables
-#song = 'twinkle.mid' #twinkle is monophonic in C maj
-#song = 'twinkle-twinkle-little-star.mid' #twinkle-twinkle-little-star is polyphonic
-#song='OnlineMidi.mid' #song in d minor
-song="GMaj.mid"
+#----------TODO----------
+# figure out how to assign minecraft pitch number to notes in file
+
+#----SONGS----
+#songs in other keys
+# 0.4875 timing error may be due to online midi website
+song='OnlineMidi.mid' #song in d minor
+#song='CMajScale.mid'
+
+
+# unacceptable songs
 #song='ID - Nyan Cat.mid' # polyphonic
-#songs that don't need to be converted
-#song="EflatMin.mid"
-#song="Fsharp.mid"
+
+
+#-----GLOBAL VARIABLES-----
+
+songName = song[:-4] # name of song
+
+# Use notes instead of MC Note numbers?
+# True yes, False no
+notesNotation = True
+
+# Choose instrument
+inst = "bass"
+
+# Choose volume
+volume = 3
 
 # MIDI note values
 noteVals = {"C":(0, 12, 24, 36, 48, 60, 72, 84, 96, 108, 120),
@@ -42,56 +61,71 @@ noteVals = {"C":(0, 12, 24, 36, 48, 60, 72, 84, 96, 108, 120),
             "B":(11, 23, 35, 47, 59, 71, 83, 95, 107, 119)
            }
 
-# Minecraft Note Block Values
-#mcNotes = 
-
+#-----FUNCTIONS-----
+def parseMidiTime():
+  #rplaces = 4 # num of decimal places to round combined time
+  timings = []
+    #parse midi file
+  for msg in MidiFile(song):
+    state = msg.type # state is type of message
+    #print(msg)
+    # check if state is an on or off signal
+    if state=="note_on": 
+      noteOnTime = msg.time
+      #print(noteOnTime)
+    if state=="note_off":
+      combinedTime = (noteOnTime + msg.time)
+      #print(combinedTime)
+      #add together on and off times for total note duration
+      timings.append(combinedTime) 
+  return timings
+  
 def checkPhonic(): #check if file is monophonic
   lastState = "note_off" # init last state
   for msg in MidiFile(song): #iterate messages
-      if not msg.is_meta: # only check non-metadata
-          state = msg.type # state is type of message
-          # check if state is an on or off signal
-          if state=="note_on" or state=="note_off":
-            # compare with last state to prevent 2 on signals
-            # being active at the same time (polyphonic)
-            if state != lastState:
-              lastState = state
-            else:
-              return False #not monophonic
+    state = msg.type # state is type of message
+    # check if state is an on or off signal
+    if state=="note_on" or state=="note_off":
+      # compare with last state to prevent 2 on signals
+      # being active at the same time (polyphonic)
+      if state != lastState:
+        lastState = state
+      else:
+        return False #not monophonic
   return True # monophonic
 
 def checkRange(): # Check if highest and lowest note values are within 25 notes
-  
   lowest = 128
   highest = -1
-  for msg in MidiFile(song): #iterate through all non-meta messages
-    if not msg.is_meta:
-      state = msg.type
-      if state=="note_on":
-        note = msg.note
-        if note < lowest:
-          lowest = note
-        if note > highest:
-          highest = note
+  #iterate through all non-meta messages
+  for msg in MidiFile(song): 
+    state = msg.type
+    if state=="note_on":
+      note = msg.note
+      # update lowest or highest note
+      if note < lowest:
+        lowest = note
+      if note > highest:
+        highest = note
+  #calculate if note range meets specs
   if (highest-lowest) <= 25:
     return True
   else:
     return False
 
 
-def scaleRange():
-  print(song)
+def scaleRange(): # reports highest and lowest note values
   lowest = 128
   highest = 0
+  #iterate through all non-meta messages
   for msg in MidiFile(song):
-    if not msg.is_meta:
-      state = msg.type
-      if state=="note_on" or state=="note_off":
-        note = msg.note
-        if note < lowest:
-          lowest = note
-        if note > highest:
-          highest = note
+    state = msg.type
+    if state=="note_on":
+      note = msg.note
+      if note < lowest:
+        lowest = note
+      if note > highest:
+        highest = note
   for n in noteVals:
     if lowest in noteVals[n]:
       print(n, lowest)
@@ -99,69 +133,84 @@ def scaleRange():
       print(n, highest)
   print("highest:",highest,"lowest:",lowest)
   
-def convKey(): #TODO change key to G flat (aka F# or mc 0) major and rel minor
-  #adapted snippet from https://gist.github.com/aldous-rey/68c6c43450517aa47474#file-transposer-py
+def convSong(): 
   score = music21.converter.parse(song)
   key = score.analyze('key')
-  print("found", key)
+  print("found", key,"\n")
   keyName = str(key)
   keyName = keyName[:2].upper() + keyName[2:]
-  if (keyName != "F# major") and (keyName != "E- minor"):
-    print("Converting key")
-    newSong = writeConvFile(key, score)
-    #newScore = music21.converter.parse(newSong)
-    #newKey = newScore.analyze('key')
-    #print("found", newKey)
-    return newSong
+  return writeConvList(key, score)
 
-  else:
-    print("Key does not need to be converted")
-    return song
-
-def writeConvFile(k, s):
-  key = k
-  score = s
-  majors = dict([("A-", -2),("A", -3),("B-", -4),("B", -5),("C", -6),("D-", 5),("D", 4),("E-", 3),("E", 2),("F", 1),("F#", 0),("G", -1)])
-  minors = dict([("G#", -5),("A", -6),("B-", 5),("B", 4),("C", 3),("D-", 2),("D", 1),("E-", 0),("E", -1),("F", -2),("G-", -3),("G", -4)])
+def writeConvList(key, score):
+  notes = []
+  octs = []
+  majors = dict([("A-", -2),("A", -3),
+                 ("B-", -4),("B", -5),
+                 ("C", -6),("D-", 5),
+                 ("D", 4),("E-", 3),
+                 ("E", 2),("F", 1),
+                 ("F#", 0),("G", -1)])
+  minors = dict([("G#", -5),("A", -6),
+                 ("B-", 5),("B", 4),
+                 ("C", 3),("D-", 2),
+                 ("D", 1),("E-", 0),
+                 ("E", -1),("F", -2),
+                 ("G-", -3),("G", -4)])
   if key.mode == "major":
     halfSteps = majors[key.tonic.name]    
   elif key.mode == "minor":
     halfSteps = minors[key.tonic.name]
   newscore = score.transpose(halfSteps)
-  file = song[:-4] + " MC converted.mid"
-  newscore.write("midi", file)
-  return file
+  timings = parseMidiTime()
+  
+  for n in newscore.flat.notes:
+    if "-" in n.pitch.name:
+      notes.append([n.pitch.name[0]+"b"])
+    else:
+      notes.append([n.pitch.name])
+    octs.append(n.octave)
+    
+  m = min(octs)
+  for i in range(len(notes)):
+    if octs[i] == m:
+      notes[i][0]+="1"
+    elif octs[i] == (m+1):
+      notes[i][0]+="2"
+    else:
+      notes[i][0]+="3"
+  
+  # TODO add octave number to pitch name
+  for x in range(len(notes)):
+    notes[x].append(timings[x])
+  return formatSongFile(notes)
   
 
-def parseMidi():
-    #parse midi file
-  for msg in MidiFile(song):
-    if not msg.is_meta: # only check non-metadata
-      state = msg.type # state is type of message
-      # check if state is an on or off signal
-      if state=="note_on" or state=="note_off":
-        #check which note corresponds to the midi value
-        for n in noteVals:
-          if msg.note in noteVals[n]:
-            print(n, msg.note)
-
-
-
+  #print(notes)
+def formatSongFile(notes):
+  formSong = "--Converted track %s \n\ntrack = {}\n\n"%songName
+  for x in range(len(notes)):
+    formSong += "track[%i] = {\"%s\", %d, \"%s\", %s}\n"%(x+1, inst, volume, str(notes[x][0]), str(notes[x][1]))
+  formSong += "\nmessage = {\"%s\", track, 1, %s}"%(songName,           
+              str(notesNotation))
+  return(formSong)
 
 def main():
-  global song
-  
+  # check if midi is monophonic and in a 25 note range
   phonic = checkPhonic()
   range = checkRange()
   if phonic and range:
-    print("song meets specifications")
-    parseMidi()
-    song = convKey()
-    print(song)
-    #parseMidi()
-    #convKey()
-    scaleRange()
-    
+    print("song meets specifications\n")
+    formattedSong = convSong()
+    print("File preview below:\n"+("---"*5)+"\n\n")
+    print(formattedSong)
+    if not os.path.exists("./converted-songs"):
+      os.mkdir("./converted-songs")
+    convertedFile = open(("./converted-songs/" + songName + 
+                          "-converted.lua"), "w")
+    convertedFile.write(formattedSong)
+    convertedFile.close()
+    print("File written")
+
   # if song does not meet specs            
   else:
     print("song does not meet specifications")
@@ -169,9 +218,5 @@ def main():
       print("song is not monophonic")
     if not range:
       print("song has too large of a note range")
-  
-  
-  # TODO figure out lowest note to put as lower octave in mc scale
-  # and fit rest of scale accordingly
 
 main()
