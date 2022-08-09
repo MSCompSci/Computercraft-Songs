b2802f00e81a96989e2c5c0fd99aea7a9f3fad9d
MS <62676614+MSCompSci@users.noreply.github.com>
2022-08-09 18:18:31 -0400

    Add converted midi file for testing

---
+++ songs/CMajScale-converted.lua
@@ -0,0 +1,14 @@
+--Converted track CMajScale 
+
+track = {}
+
+track[1] = {"bass", 3, "F#", 0.4875}
+track[2] = {"bass", 3, "G#", 0.5}
+track[3] = {"bass", 3, "Bb", 0.5}
+track[4] = {"bass", 3, "B", 0.5}
+track[5] = {"bass", 3, "C#", 0.5}
+track[6] = {"bass", 3, "Eb", 0.5}
+track[7] = {"bass", 3, "F", 0.5}
+track[8] = {"bass", 3, "F#", 0.5}
+
+message = {"CMajScale", track, 1, True}
