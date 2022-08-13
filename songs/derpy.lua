98e97bdd77e4d9e3008fdc159f2b0d0d10516c0a
MS <62676614+MSCompSci@users.noreply.github.com>
2022-08-13 10:15:56 -0400

    Update derpy to new standard
    Add speaker numbers, change array appending method to track[i], change "song" to "message", add tempo multiplier and notation style bool

--- songs/derpy.lua
+++ songs/derpy.lua
@@ -1,13 +1,14 @@
+-- Demo track derpy
 track = {}
-table.insert(track, {"pling", 3, 24, .25})
-table.insert(track, {"bass", 3, 22, .25})
-table.insert(track, {"pling", 3, 24, .20})
-table.insert(track, {"bass", 3, 22, .18})
-table.insert(track, {"pling", 3, 24, .125})
-table.insert(track, {"guitar", 3, 12, .125})
-table.insert(track, {"pling", 3, 22, .125})
-table.insert(track, {"pling", 3, 21, .125})
-table.insert(track, {"pling", 3, 20, .125})
-table.insert(track, {"pling", 3, 19, .125})
-table.insert(track, {"pling", 3, 18, .125})
-song = {"derpy", track}
+track[1] = {"pling", 3, 24, .25, 1}
+track[2] = {"bass", 3, 22, .25, 1}
+track[3] = {"pling", 3, 24, .20, 1}
+track[4] = {"bass", 3, 22, .18, 1}
+track[5] = {"pling", 3, 24, .125, 1}
+track[6] = {"guitar", 3, 12, .125, 1}
+track[7] = {"pling", 3, 22, .125, 1}
+track[8] = {"pling", 3, 21, .125, 1}
+track[9] = {"pling", 3, 20, .125, 1}
+track[10] = {"pling", 3, 19, .125, 1}
+track[11] = {"pling", 3, 18, .125, 1}
+message = {"derpy", track, 1, false}
