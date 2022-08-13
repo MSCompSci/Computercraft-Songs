fbe51b1f1f13d9b02b54e0c028aaa9923ba678dd
MS <62676614+MSCompSci@users.noreply.github.com>
2022-08-13 10:04:13 -0400

    Add speaker number

--- songs/bell.lua
+++ songs/bell.lua
@@ -1,8 +1,9 @@
+-- Demo track
 track = {}
-track[1] = {"bass", 3, 24, .25}
-track[2] = {"bass", 3, 16, .25}
-track[3] = {"bass", 3, 28, .25}
-track[4] = {"flute", 3, 4, .25}
-track[5] = {"bass", 3, 24, .25}
-track[6] = {"bass", 3, 24, .25}
+track[1] = {"bass", 3, 24, .25, 1}
+track[2] = {"bass", 3, 16, .25, 1}
+track[3] = {"bass", 3, 28, .25, 1}
+track[4] = {"flute", 3, 4, .25, 1}
+track[5] = {"bass", 3, 24, .25, 1}
+track[6] = {"bass", 3, 24, .25, 1}
 song = {"bell", track}
